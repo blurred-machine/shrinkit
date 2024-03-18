6de243d5a661bf6bd1b5131896fc7033a8556018
blurred-machine <paras.varshney97@gmail.com>
2024-03-17 21:13:27 -0400

    updated requirements

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ URL = 'https://github.com/blurred-machine/shrinkit'
 EMAIL = 'paras.varshney97@gmail.com'
 AUTHOR = 'Paras Varshney'
 REQUIRES_PYTHON = '>=3.10.0'
-VERSION = '0.1.8'
+VERSION = '0.1.9'
 
 REQUIRED = [
-    'streamlit', 'pandas', 'numpy', 'scipy', 'scikit-learn', 'ydata-profiling'
+    'streamlit', 'pandas', 'numpy', 'scipy', 'scikit-learn', 'ydata-profiling', 'streamlit_ydata_profiling'
 ]
 
 # What packages are optional?
