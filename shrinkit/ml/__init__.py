1714603b31f8f35901f951dec44ade44a407fcbf
blurred-machine <paras.varshney97@gmail.com>
2024-03-17 20:08:58 -0400

    updated

--- shrinkit/ml/__init__.py
+++ shrinkit/ml/__init__.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-from shrinkit.ml.imputation import *
-from shrinkit.ml.filtration import *
-from shrinkit.ml.machine_learning import *
-from shrinkit.ml.encoding import *
-from shrinkit.ml.normalization import *
-from shrinkit.ml.evaluation import *
+from ml.imputation import *
+from ml.filtration import *
+from ml.machine_learning import *
+from ml.encoding import *
+from ml.normalization import *
+from ml.evaluation import *
